03652d1c13d22239d216ce0f776a13c10957f2cf
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-21 00:11:17 +0100

    Update media_cats.js

--- assets/media_cats.js
+++ assets/media_cats.js
@@ -2,7 +2,19 @@
  * JavaScript für das media_cats AddOn
  */
 $(document).ready(function() {
-    // Wir verwenden das Standard-Bootstrap 3 Akkordeon, kein eigenes JavaScript nötig
+    // Bootstrap-Kollapse-Events behandeln
+    $('.panel-collapse').on('show.bs.collapse', function () {
+        // Formularelemente beim Öffnen aktivieren
+        $(this).find('input, select, textarea, button').prop('disabled', false);
+    });
+    
+    $('.panel-collapse').on('hide.bs.collapse', function () {
+        // Formularelemente beim Schließen deaktivieren
+        $(this).find('input, select, textarea, button').prop('disabled', true);
+    });
+    
+    // Initial alle Formularelemente in geschlossenen Panels deaktivieren
+    $('.panel-collapse:not(.in)').find('input, select, textarea, button').prop('disabled', true);
     
     // Bestätigungsdialog vor dem Speichern
     $('form button[name="save_category"]').on('click', function(e) {
@@ -30,5 +42,18 @@ $(document).ready(function() {
     
     $('select[name="parent_id"]').each(function() {
         $(this).data('original', $(this).val());
+    });
+    
+    // Zusätzlich: Bei Formularabsendung alle nicht sichtbaren Formulare deaktivieren
+    $('form').on('submit', function() {
+        // Sicherstellen, dass nur Formulare in geöffneten Panels gesendet werden
+        if (!$(this).closest('.panel-collapse').hasClass('in')) {
+            return false; // Absenden verhindern, falls Panel geschlossen
+        }
+        
+        // Für mehr Sicherheit: auch hier alle Elemente in geschlossenen Panels deaktivieren
+        $('.panel-collapse:not(.in)').find('input, select, textarea, button').prop('disabled', true);
+        
+        return true;
     });
 });
