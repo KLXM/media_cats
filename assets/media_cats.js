761e1ea5ecf63a81e7523fde39ccf5fe32f5383a
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-20 23:45:34 +0100

    Update media_cats.js

--- assets/media_cats.js
+++ assets/media_cats.js
@@ -2,27 +2,29 @@
  * JavaScript für das media_cats AddOn
  */
 $(document).ready(function() {
-    // Bestätigungsdialoge für kritische Aktionen
-    $('form button[name="confirm_restore"], form button[name="confirm_delete"]').on('click', function(e) {
-        if (!confirm('Sind Sie sicher, dass Sie diese Aktion ausführen möchten?')) {
-            e.preventDefault();
-            return false;
-        }
+    // Panel öffnen wenn auf den Header geklickt wird
+    $('.panel-heading').on('click', function(e) {
+        var collapseId = $(this).find('a').attr('href');
+        
+        // Alle anderen Panel-Inhalte schließen
+        $('.panel-collapse').not(collapseId).removeClass('in');
+        
+        // Gewähltes Panel umschalten
+        $(collapseId).toggleClass('in');
+        
+        e.preventDefault();
     });
     
-    // Speichern-Bestätigung
-    $('form button[name="save"]').on('click', function(e) {
-        // Prüfen, ob Änderungen vorgenommen wurden
-        var hasChanges = false;
-        $('input[name^="category_name"], select[name^="parent_id"]').each(function() {
-            if ($(this).data('original') !== $(this).val()) {
-                hasChanges = true;
-                return false; // Schleife abbrechen, wenn Änderung gefunden
-            }
-        });
+    // Bestätigungsdialog vor dem Speichern
+    $('form button[name="save_category"]').on('click', function(e) {
+        var categoryName = $(this).closest('form').find('input[name="category_name"]').val();
+        var parentId = $(this).closest('form').find('select[name="parent_id"]').val();
+        var originalName = $(this).closest('form').find('input[name="category_name"]').data('original');
+        var originalParentId = $(this).closest('form').find('select[name="parent_id"]').data('original');
         
-        if (hasChanges) {
-            if (!confirm('Möchten Sie die Änderungen speichern?')) {
+        // Prüfen, ob sich etwas geändert hat
+        if (categoryName !== originalName || parseInt(parentId) !== parseInt(originalParentId)) {
+            if (!confirm(rex.media_cats_confirm_save)) {
                 e.preventDefault();
                 return false;
             }
@@ -30,7 +32,22 @@ $(document).ready(function() {
     });
     
     // Original-Werte speichern für Änderungserkennung
-    $('input[name^="category_name"], select[name^="parent_id"]').each(function() {
+    $('input[name="category_name"]').each(function() {
         $(this).data('original', $(this).val());
     });
+    
+    $('select[name="parent_id"]').each(function() {
+        $(this).data('original', $(this).val());
+    });
+    
+    // Wenn Hash in URL vorhanden, entsprechendes Panel öffnen
+    var hash = window.location.hash;
+    if (hash && hash.match(/^#collapse-\d+$/)) {
+        $(hash).addClass('in');
+        
+        // Zu dem Panel scrollen
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 100
+        }, 500);
+    }
 });
