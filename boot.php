acb3f3b8a2b6bb1f269963a69e2ad6a1ac7a4d52
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-20 23:00:11 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,1 +1,15 @@
-<?php // Boot code
+<?php
+
+// Autoloader für den Namespace KLXM\MediaCats
+rex_autoload::addDirectory(rex_path::addon('media_cats', 'lib'));
+
+// Addon-Assets (CSS/JS) einbinden, wenn wir im Backend sind und die Addon-Seite aktiv ist
+if (rex::isBackend() && rex::getUser()) {
+    if (rex_be_controller::getCurrentPagePart(1) == 'media_cats') {
+        // CSS einbinden
+        rex_view::addCssFile(rex_url::addonAssets('media_cats', 'media_cats.css'));
+        
+        // JavaScript einbinden
+        rex_view::addJsFile(rex_url::addonAssets('media_cats', 'media_cats.js'));
+    }
+}
