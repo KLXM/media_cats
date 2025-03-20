0c6c4fded126656b9ce7118849a4057482804e3e
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-20 23:00:26 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,8 +1,5 @@
 <?php
 
-// Autoloader für den Namespace KLXM\MediaCats
-rex_autoload::addDirectory(rex_path::addon('media_cats', 'lib'));
-
 // Addon-Assets (CSS/JS) einbinden, wenn wir im Backend sind und die Addon-Seite aktiv ist
 if (rex::isBackend() && rex::getUser()) {
     if (rex_be_controller::getCurrentPagePart(1) == 'media_cats') {
