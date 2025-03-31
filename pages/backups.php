2f3be868fc3233e6c3a0617ae948b755312f598a
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-31 13:44:28 +0200

    Update backups.php

--- pages/backups.php
+++ pages/backups.php
@@ -36,7 +36,7 @@ if (rex_post('restore', 'boolean') && $csrfToken->isValid()) {
                     ' . $csrfToken->getHiddenField() . '
                     <p>' . rex_i18n::msg('media_cats_restore_confirm', rex_escape($filename)) . '</p>
                     <button class="btn btn-warning" type="submit" name="confirm_restore" value="1">' . rex_i18n::msg('media_cats_restore_confirm_button') . '</button>
-                    <a class="btn btn-abort" href="' . rex_url::currentBackendPage() . '">' . rex_i18n::msg('media_cats_cancel_button') . '</a>
+                    <a class="btn btn-save" href="' . rex_url::currentBackendPage() . '">' . rex_i18n::msg('media_cats_cancel_button') . '</a>
                 </form>
             ';
         }
